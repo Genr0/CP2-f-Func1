8f8401fd0b17b7c12f6b13ff643ad74e36a9dbda
Genr0 <155598812+Genr0@users.noreply.github.com>
2024-01-04 12:53:10 +0800

    Add files via upload

--- Proj1.cpp
+++ Proj1.cpp
@@ -1,31 +1,28 @@
 #include <iostream>
 using namespace std;
-
-int main()
+int MainMenu() {
+    cout
+        << "--- Please Select for Math Solution to use ---\n"
+        << "1. Add\n"
+        << "2. Subtract\n"
+        << "3. Multipliction\n"
+        << "4. Division\n"
+        << "\n"
+        << "Enter Choice: ";
+    return 0;
+}
+int Calculatorings()
 {
-    int val1, val2, choice;
+    int choice, val1, val2;
     char continueChoice = 'y';
-
-    do
-    {
-        do
-        {
-            cout
-                << "--- Please Select for Math Solution to use ---\n"
-                << "1. Add\n"
-                << "2. Subtract\n"
-                << "3. Multipliction\n"
-                << "4. Division\n"
-                << "\n"
-                << "Enter Choice: ";
+    do {
+        do {
             cin >> choice;
-
             system("cls");
-
             switch (choice)
             {
             case 1:
-              cout << "Please input value 1:";
+                cout << "Please input value 1:";
                 cin >> val1;
                 cout << "Please input value 2:";
                 cin >> val2;
@@ -53,7 +50,7 @@ int main()
                 cout << val1 << " / " << val2 << " = " << val1 / val2 << "\n";
                 break;
             default:
-               cout << "User Input Incorrect\n\n";
+                cout << "User Input Incorrect\n\n";
                 break;
             }
         } while (choice < 0 || choice > 5);
@@ -64,4 +61,12 @@ int main()
         system("cls");
 
     } while (continueChoice == 'y');
+    return 0;
 }
+int main()
+{
+    MainMenu();
+    Calculatorings();
+}
+
+ 
